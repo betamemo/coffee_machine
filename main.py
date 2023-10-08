18d36b05be4d2547bf0fe9bc5d28c722300762bc
BK <betamemo@BK.local>
2023-10-08 23:15:46 +0700

    Update

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ coffee_maker = CoffeeMaker()
 is_on = True
 
 while is_on:
-    choice = input('What would you like?: ') or 'latte'
-
-    drink = menu.find_drink(choice)
-    if coffee_maker.is_resource_sufficient(drink):  # check resources sufficient
-        if money_machine.make_payment(drink.cost):  # insert coins
-            coffee_maker.report()
-            money_machine.report()
-            coffee_maker.make_coffee(drink)  # make coffee
-
-    if choice == 'off':
-        print('Turn off the coffee maker.')
-        break
+    choice = input('What would you like? (espresso/latte/flat white): ') or 'latte'
 
     if choice == 'report':
         coffee_maker.report()  # show the current resource of coffee maker.
+    elif choice == 'off':
+        print('Turn off the coffee maker.')
+        is_on = False
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):  # check resources sufficient
+            if money_machine.make_payment(drink.cost):  # insert coins
+                coffee_maker.report()
+                money_machine.report()
+                coffee_maker.make_coffee(drink)  # make coffee
